c634494cdd4bdfe0916c1698cb3f6e9b6d079c5f
Gevorg <61474836+Gevorg22@users.noreply.github.com>
2021-01-05 10:39:05 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-document.querySelector('.header__burger').addEventListener('click', () => {
-    document.querySelector('.nav__list').classList.toggle('active');
-    document.querySelector('.header__burger').classList.toggle('active');
-    document.body.classList.toggle('lock');
+$( ".header__burger" ).on( "click", function() {
+    $('.nav__list').toggleClass('active');
+    $('.header__burger').toggleClass('active');
+    $( "body" ).toggleClass('lock');
 });
 
-document.querySelector('.header__burger-desktop').addEventListener('click', () => {
-    document.querySelector('.nav__list').classList.toggle('active');
-    document.querySelector('.header__burger-desktop').classList.toggle('active');
-    document.body.classList.toggle('lock');
+$( ".header__burger-desktop" ).on( "click", function() {
+    $('.nav__list').toggleClass('active');
+    $('.header__burger-desktop').toggleClass('active');
+    $( "body" ).toggleClass('lock');
 });
